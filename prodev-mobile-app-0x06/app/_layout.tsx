71eb4d3410f90c77ac90c6ba7fa6abdc67b1d263
Tsegaw-Mao <tsegawjohnj@gmail.com>
2025-09-10 18:53:14 +0300

    task 2

--- prodev-mobile-app-0x06/app/_layout.tsx
+++ prodev-mobile-app-0x06/app/_layout.tsx
@@ -1,14 +1,28 @@
-import { Stack } from "expo-router";
-import { View } from 'react-native'
+import { AntDesign, MaterialIcons, Octicons  } from "@expo/vector-icons";
+import { Tabs } from "expo-router";
 
-export default function RootLayout() {
-  return (
-    <Stack screenOptions={{
-      headerShown: false,
-    }}>
-      <Stack.Screen name="index" />
-      <Stack.Screen name="join" />
-      <Stack.Screen name="signin" />
-    </Stack>
-  );
+const HomeRootLayout = () => {
+    return (
+        <Tabs screenOptions={{
+            tabBarActiveTintColor: '#34967C',
+            headerShown: false
+        }}>
+            <Tabs.Screen name="index" options={{
+                title: 'Home',
+                tabBarIcon: ({ color }) => <AntDesign name="home" size={24} color={color} />,
+             }} />             
+             <Tabs.Screen name="join" options={{
+                title: 'Join',
+                headerShown: false,
+                tabBarIcon: ({ color }) => <MaterialIcons name="create" size={24} color={color} />,
+            }} />
+            <Tabs.Screen name="signin" options={{
+                title: 'Signin',
+                headerShown: false,
+                tabBarIcon: ({ color }) => <Octicons name="sign-in" size={27} color={color} />
+            }} />
+        </Tabs>
+    )
 }
+
+export default HomeRootLayout;
