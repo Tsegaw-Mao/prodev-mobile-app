b899518c4035d9cd9abfcb0e39ea527647fe26bd
Tsegaw-Mao <tsegawjohnj@gmail.com>
2025-09-10 18:27:17 +0300

    task 2

---
+++ prodev-mobile-app-0x06/styles/index.ts
@@ -0,0 +1,8 @@
+// styles/index.ts
+import { styles as mainStyles } from "./_mainstyle";
+import { styles as joinStyles } from "./_join";
+
+export const styles = {
+  ...mainStyles,
+  ...joinStyles,
+};
